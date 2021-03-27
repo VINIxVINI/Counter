d831aabbd734a75731ff9c930d4fed509d1bd077
admin <admin@SELF>
2021-03-27 11:52:28 +0300

    Add project files.

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿/* Initializing counter */
+let count = 0
+
+// selecting buttons using btn class and value using id selector 
+const value = document.querySelector('#value');
+const Buttons = document.querySelectorAll('.btn');
+
+Buttons.forEach(function (btn) {
+    btn.addEventListener('click', function (viewClass) {
+        const classList = viewClass.currentTarget.classList;
+        if (classList.contains('decrease')) {
+            count--;
+            console.log(count);
+        }
+        value.textContent = count;
+        if (classList.contains('reset')) {
+            count = 0;
+
+        }
+        value.textContent = count;
+        if (classList.contains('increase')) {
+            count++;
+        }
+        if (count === 0) {
+            value.text.color = "black";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        
+
+    }); 
+    
+});
